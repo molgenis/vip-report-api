e4ffbda309efa65ccc3fc4c4826214711b6e84a1
Dennis Hendriksen <d.hendriksen@umcg.nl>
2020-06-15 15:03:49 +0200

    refactor: replace enums with allowed string lists

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-import {Items, Metadata, Params, Phenotype, ReportData, Resource, Record, Sample} from './index'
+import {Items, Metadata, Params, Phenotype, Record, ReportData, Resource, Sample} from './index'
 
 export = Api
 
@@ -8,29 +8,31 @@ declare class Api {
     getMeta(): Promise<Metadata>
 
     getRecords(params?: Params): Promise<Items<Record>>
+
     getSamples(params?: Params): Promise<Items<Sample>>
+
     getPhenotypes(params?: Params): Promise<Items<Phenotype>>
 
     get(resource: string, params?: Params): Promise<Items<Resource>>
 }
 
 declare namespace Api {
-export interface ReportData {
+    export interface ReportData {
         metadata: Metadata,
-            data: object
+        data: object
     }
 
-export interface Metadata {
+    export interface Metadata {
         app: AppMetadata
         htsFile: HtsFileMetadata
         records: RecordsMetadata
     }
 
-export interface Resource {
+    export interface Resource {
 
     }
 
-export interface Params {
+    export interface Params {
         query?: Query
         sort?: string
         desc?: boolean
@@ -38,135 +40,98 @@ export interface Params {
         size?: number
     }
 
-export interface Record extends Resource {
+    export interface Record extends Resource {
         c: string
         p: number
         i?: Array<string>
-            r: string,
-            a: Array<string>
+        r: string,
+        a: Array<string>
         q?: number
         f?: Array<string>
         n?: Object
         s?: Array<RecordSample>
     }
 
-export interface Sample extends Resource {
+    export interface Sample extends Resource {
         person: Person
         index: number
     }
 
-export interface Items<T extends Resource> {
+    export interface Items<T extends Resource> {
         items: Array<T>
-            total: number
-}
+        total: number
+    }
 
-export interface AppMetadata {
+    export interface AppMetadata {
         name: string
         version: string
         args: string
     }
 
-export interface HtsFileMetadata {
+    export interface HtsFileMetadata {
         htsFormat: string
         uri: string
         genomeAssembly: string
     }
 
-export interface RecordsMetadata {
+    export interface RecordsMetadata {
         info: Array<InfoMetadata>
     }
 
-export interface InfoMetadata {
+    export interface InfoMetadata {
         id: string
         number?: InfoNumberMetadata
-        type: InfoType
+        type: 'CHARACTER' | 'INTEGER' | 'FLAG' | 'FLOAT' | 'STRING'
         description: string
         source?: string
         version?: string
     }
 
-export interface InfoNumberMetadata {
-        type: NumberType
+    export interface InfoNumberMetadata {
+        type: 'NUMBER' | 'PER_ALT' | 'PER_ALT_AND_REF' | 'PER_GENOTYPE' | 'OTHER'
         count?: number
     }
 
-export enum InfoType {
-        CHARACTER = "CHARACTER",
-            INTEGER = "INTEGER",
-            FLAG = "FLAG",
-            FLOAT = "FLOAT",
-            STRING = "STRING"
-    }
-
-export enum NumberType {
-        NUMBER = "NUMBER",
-            PER_ALT = "PER_ALT",
-            PER_ALT_AND_REF = "PER_ALT_AND_REF",
-            PER_GENOTYPE = "PER_GENOTYPE",
-            OTHER = "OTHER"
-    }
-
-export enum GenotypeType {
-        het = 'het',
-            hom_a = 'hom_a',
-            hom_r = 'hom_r',
-            miss = 'miss',
-            part = 'part'
-    }
-
-export interface Query {
+    export interface Query {
         operator: '==' | '!=' | 'in' | '!in',
-            selector: string | Array<string>
+        selector: string | Array<string>
         args: string | number | Array<string> | Array<number>
     }
 
-export interface RecordSample {
+    export interface RecordSample {
         gt: Genotype
     }
 
-export interface Genotype {
+    export interface Genotype {
         a?: Array<string>
-            p: boolean,
-            t: GenotypeType
+        p: boolean,
+        t: 'het' | 'hom_a' | 'hom_r' | 'miss' | 'part'
     }
 
-export interface Person {
+    export interface Person {
         familyId: string
         individualId: string
         paternalId: string
         maternalId: string
-        sex: Sex
-        affectedStatus: AffectedStatus
+        sex: 'UNKNOWN_SEX' | 'FEMALE' | 'MALE' | 'OTHER_SEX'
+        affectedStatus: 'MISSING' | 'UNAFFECTED' | 'AFFECTED'
     }
 
-export enum Sex {
-        UNKNOWN_SEX = 'UNKNOWN_SEX',
-            FEMALE = 'FEMALE',
-            MALE = 'MALE',
-            OTHER_SEX = 'OTHER_SEX'
-    }
-
-export enum AffectedStatus {
-        MISSING = 'MISSING',
-            UNAFFECTED = 'UNAFFECTED',
-            AFFECTED = 'AFFECTED'
-    }
-
-export interface Phenotype extends Resource {
+    export interface Phenotype extends Resource {
         subject: PhenotypeSubject
         phenotypicFeaturesList: Array<PhenotypicFeature>
     }
 
-export interface PhenotypeSubject {
+    export interface PhenotypeSubject {
         id: string
     }
 
-export interface OntologyClass {
+    export interface OntologyClass {
         id: string,
-            label: string
+        label: string
     }
 
-export interface PhenotypicFeature {
+    export interface PhenotypicFeature {
         type: OntologyClass
     }
 }
