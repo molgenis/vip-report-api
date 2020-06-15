15358cf72a49ecc382f5167da3cf07ba5693e7dc
Dennis Hendriksen <d.hendriksen@umcg.nl>
2020-06-15 16:19:40 +0200

    fix: type definition

--- index.d.ts
+++ index.d.ts
@@ -1,131 +1,135 @@
-export declare class Api {
-    constructor(reportData: ReportData)
+export = Api
 
-    getMeta(): Promise<Metadata>
+declare class Api {
+    constructor(reportData: Api.ReportData)
 
-    getRecords(params?: Params): Promise<Items<Record>>
+    getMeta(): Promise<Api.Metadata>
 
-    getSamples(params?: Params): Promise<Items<Sample>>
+    getRecords(params?: Api.Params): Promise<Api.Items<Api.Record>>
 
-    getPhenotypes(params?: Params): Promise<Items<Phenotype>>
+    getSamples(params?: Api.Params): Promise<Api.Items<Api.Sample>>
 
-    get(resource: string, params?: Params): Promise<Items<Resource>>
+    getPhenotypes(params?: Api.Params): Promise<Api.Items<Api.Phenotype>>
+
+    get(resource: string, params?: Api.Params): Promise<Api.Items<Api.Resource>>
 }
 
-export interface ReportData {
-    metadata: Metadata,
-    data: object
-}
+declare namespace Api {
+    export interface ReportData {
+        metadata: Metadata,
+        data: object
+    }
 
-export interface Metadata {
-    app: AppMetadata
-    htsFile: HtsFileMetadata
-    records: RecordsMetadata
-}
+    export interface Metadata {
+        app: AppMetadata
+        htsFile: HtsFileMetadata
+        records: RecordsMetadata
+    }
 
-export interface Resource {
+    export interface Resource {
 
-}
+    }
 
-export interface Params {
-    query?: Query
-    sort?: string
-    desc?: boolean
-    page?: number
-    size?: number
-}
+    export interface Params {
+        query?: Query
+        sort?: string
+        desc?: boolean
+        page?: number
+        size?: number
+    }
 
-export interface Record extends Resource {
-    c: string
-    p: number
-    i?: Array<string>
-    r: string,
-    a: Array<string>
-    q?: number
-    f?: Array<string>
-    n?: Object
-    s?: Array<RecordSample>
-}
+    export interface Record extends Resource {
+        c: string
+        p: number
+        i?: Array<string>
+        r: string,
+        a: Array<string>
+        q?: number
+        f?: Array<string>
+        n?: Object
+        s?: Array<RecordSample>
+    }
 
-export interface Sample extends Resource {
-    person: Person
-    index: number
-}
+    export interface Sample extends Resource {
+        person: Person
+        index: number
+    }
 
-export interface Items<T extends Resource> {
-    items: Array<T>
-    total: number
-}
+    export interface Items<T extends Resource> {
+        items: Array<T>
+        total: number
+    }
 
-export interface AppMetadata {
-    name: string
-    version: string
-    args: string
-}
+    export interface AppMetadata {
+        name: string
+        version: string
+        args: string
+    }
 
-export interface HtsFileMetadata {
-    htsFormat: string
-    uri: string
-    genomeAssembly: string
-}
+    export interface HtsFileMetadata {
+        htsFormat: string
+        uri: string
+        genomeAssembly: string
+    }
 
-export interface RecordsMetadata {
-    info: Array<InfoMetadata>
-}
+    export interface RecordsMetadata {
+        info: Array<InfoMetadata>
+    }
 
-export interface InfoMetadata {
-    id: string
-    number?: InfoNumberMetadata
-    type: 'CHARACTER' | 'INTEGER' | 'FLAG' | 'FLOAT' | 'STRING'
-    description: string
-    source?: string
-    version?: string
-}
+    export interface InfoMetadata {
+        id: string
+        number?: InfoNumberMetadata
+        type: 'CHARACTER' | 'INTEGER' | 'FLAG' | 'FLOAT' | 'STRING'
+        description: string
+        source?: string
+        version?: string
+    }
 
-export interface InfoNumberMetadata {
-    type: 'NUMBER' | 'PER_ALT' | 'PER_ALT_AND_REF' | 'PER_GENOTYPE' | 'OTHER'
-    count?: number
-}
+    export interface InfoNumberMetadata {
+        type: 'NUMBER' | 'PER_ALT' | 'PER_ALT_AND_REF' | 'PER_GENOTYPE' | 'OTHER'
+        count?: number
+    }
 
-export interface Query {
-    operator: '==' | '!=' | 'in' | '!in',
-    selector: string | Array<string>
-    args: string | number | Array<string> | Array<number>
-}
+    export interface Query {
+        operator: '==' | '!=' | 'in' | '!in',
+        selector: string | Array<string>
+        args: string | number | Array<string> | Array<number>
+    }
 
-export interface RecordSample {
-    gt: Genotype
-}
+    export interface RecordSample {
+        gt: Genotype
+    }
 
-export interface Genotype {
-    a?: Array<string>
-    p: boolean,
-    t: 'het' | 'hom_a' | 'hom_r' | 'miss' | 'part'
-}
+    export interface Genotype {
+        a?: Array<string>
+        p: boolean,
+        t: 'het' | 'hom_a' | 'hom_r' | 'miss' | 'part'
+    }
 
-export interface Person {
-    familyId: string
-    individualId: string
-    paternalId: string
-    maternalId: string
-    sex: 'UNKNOWN_SEX' | 'FEMALE' | 'MALE' | 'OTHER_SEX'
-    affectedStatus: 'MISSING' | 'UNAFFECTED' | 'AFFECTED'
-}
+    export interface Person {
+        familyId: string
+        individualId: string
+        paternalId: string
+        maternalId: string
+        sex: 'UNKNOWN_SEX' | 'FEMALE' | 'MALE' | 'OTHER_SEX'
+        affectedStatus: 'MISSING' | 'UNAFFECTED' | 'AFFECTED'
+    }
 
-export interface Phenotype extends Resource {
-    subject: PhenotypeSubject
-    phenotypicFeaturesList: Array<PhenotypicFeature>
-}
+    export interface Phenotype extends Resource {
+        subject: PhenotypeSubject
+        phenotypicFeaturesList: Array<PhenotypicFeature>
+    }
 
-export interface PhenotypeSubject {
-    id: string
-}
+    export interface PhenotypeSubject {
+        id: string
+    }
 
-export interface OntologyClass {
-    id: string,
-    label: string
-}
+    export interface OntologyClass {
+        id: string,
+        label: string
+    }
 
-export interface PhenotypicFeature {
-    type: OntologyClass
+    export interface PhenotypicFeature {
+        type: OntologyClass
+    }
 }
