c2ed8c121c31f07804e04c27764dc03b73428314
dennishendriksen <d.hendriksen@umcg.nl>
2025-11-03 14:53:35 +0100

    refactor: WindowApiClientFactory

--- src/apiClient.ts
+++ src/apiClient.ts
@@ -2,6 +2,7 @@ import { VcfMetadata, VcfRecord } from "@molgenis/vip-report-vcf";
 import {
   Api,
   AppMetadata,
+  BinaryReportData,
   Cram,
   DecisionTree,
   Item,
@@ -10,47 +11,33 @@ import {
   Params,
   Phenotype,
   RecordParams,
-  ReportData,
   Resource,
   Sample,
 } from "./index";
-import { SqlLoader } from "./SqlLoader";
+import { ReportDatabase } from "./ReportDatabase";
 import { validateQuery } from "./validateQuery";
 import { DatabaseRecord, DatabaseResource, TableSize } from "./sql";
-import { Database } from "sql.js";
 
 export class ApiClient implements Api {
-  private reportData: ReportData;
-  private loader: Promise<SqlLoader>;
-
-  constructor(reportData: ReportData, db: Promise<Database>) {
-    this.reportData = reportData;
-    const loader = new SqlLoader();
-    this.loader = loader.init(db);
-  }
+  constructor(
+    private readonly db: ReportDatabase,
+    private readonly binary: BinaryReportData,
+  ) {}
 
   async getConfig(): Promise<Json | null> {
-    const loader = await this.loader;
-    if (!loader) throw new Error("Loader was not initialized.");
-    return loader.loadConfig();
+    return this.db.loadConfig();
   }
 
   async getRecordsMeta(): Promise<VcfMetadata> {
-    const loader = await this.loader;
-    if (!loader) throw new Error("Loader was not initialized.");
-    const meta = loader.getMetadata();
-    if (!meta) throw new Error("Metadata not loaded.");
-    return meta;
+    return this.db.getMetadata();
   }
 
   async getRecords(params: RecordParams = {}): Promise<PagedItems<VcfRecord>> {
-    const loader = await this.loader;
-    if (!loader) throw new Error("Loader was not initialized.");
     const page = params.page ?? 0;
     const size = params.size ?? 10;
-    validateQuery(loader.getMetadata(), params.query);
-    const tableSize: TableSize = loader.countMatchingVariants(params.query);
-    const variants: DatabaseRecord[] = loader.loadVcfRecords(page, size, params.sort, params.query, params.sampleIds);
+    validateQuery(this.db.getMetadata(), params.query);
+    const tableSize: TableSize = this.db.countMatchingVariants(params.query);
+    const variants: DatabaseRecord[] = this.db.loadVcfRecords(page, size, params.sort, params.query, params.sampleIds);
     return this.toPagedItems(variants, page, size, tableSize.size, tableSize.totalSize) as PagedItems<VcfRecord>;
   }
 
@@ -77,37 +64,29 @@ export class ApiClient implements Api {
   }
 
   async getRecordById(id: number, sampleIds: number[] | undefined = undefined): Promise<Item<VcfRecord>> {
-    const loader = await this.loader;
-    if (!loader) throw new Error("Loader was not initialized.");
-    const record: VcfRecord = loader.loadVcfRecordById(id, sampleIds);
+    const record: VcfRecord = this.db.loadVcfRecordById(id, sampleIds);
     return { data: record, id: id };
   }
 
   async getSamples(params: Params = {}): Promise<PagedItems<Sample>> {
     const page = params.page ?? 0;
     const size = params.size ?? 10;
-    const loader = await this.loader;
-    if (!loader) throw new Error("Loader was not initialized.");
-    const tableSize: TableSize = loader.countMatchingSamples(params.query);
-    const samples = loader.loadSamples(page, size, params.query);
+    const tableSize: TableSize = this.db.countMatchingSamples(params.query);
+    const samples = this.db.loadSamples(page, size, params.query);
     return this.toPagedItems(samples, page, size, tableSize.size, tableSize.totalSize) as PagedItems<Sample>;
   }
 
   async getSampleById(id: number): Promise<Item<Sample>> {
-    const loader = await this.loader;
-    if (!loader) throw new Error("Loader was not initialized.");
-    const sample: Sample = loader.loadSampleById(id);
+    const sample: Sample = this.db.loadSampleById(id);
     return { data: sample, id: id };
   }
 
   async getPhenotypes(params: Params = {}): Promise<PagedItems<Phenotype>> {
     const page = params.page ?? 0;
     const size = params.size ?? 10;
-    const loader = await this.loader;
-    if (!loader) throw new Error("Loader was not initialized.");
-    const tableSize = loader.countMatchingPhenotypes(params.query);
+    const tableSize = this.db.countMatchingPhenotypes(params.query);
     return this.toPagedItems(
-      loader.loadPhenotypes(page, size, params.query),
+      this.db.loadPhenotypes(page, size, params.query),
       page,
       size,
       tableSize.size,
@@ -117,8 +96,8 @@ export class ApiClient implements Api {
 
   getFastaGz(contig: string, pos: number): Promise<Uint8Array | null> {
     let buffer: Uint8Array | null = null;
-    if (this.reportData.binary.fastaGz) {
-      for (const [key, value] of Object.entries(this.reportData.binary.fastaGz)) {
+    if (this.binary.fastaGz) {
+      for (const [key, value] of Object.entries(this.binary.fastaGz)) {
         const pair = key.split(":");
         if (pair[0] === contig) {
           const interval = pair[1]!.split("-");
@@ -133,31 +112,25 @@ export class ApiClient implements Api {
   }
 
   async getAppMetadata(): Promise<AppMetadata> {
-    const loader = await this.loader;
-    if (!loader) throw new Error("Loader was not initialized.");
-    return loader.loadAppMetadata();
+    return this.db.loadAppMetadata();
   }
 
   getGenesGz(): Promise<Uint8Array | null> {
-    const genesGz = this.reportData.binary.genesGz;
+    const genesGz = this.binary.genesGz;
     return Promise.resolve(genesGz ?? null);
   }
 
   getCram(sampleId: string): Promise<Cram | null> {
-    const cram = this.reportData.binary.cram;
+    const cram = this.binary.cram;
     const sampleCram = cram ? (cram[sampleId] ?? null) : null;
     return Promise.resolve(sampleCram);
   }
 
   async getDecisionTree(): Promise<DecisionTree | null> {
-    const loader = await this.loader;
-    if (!loader) throw new Error("Loader was not initialized.");
-    return loader.loadDecisionTree("decisionTree");
+    return this.db.loadDecisionTree("decisionTree");
   }
 
   async getSampleTree(): Promise<DecisionTree | null> {
-    const loader = await this.loader;
-    if (!loader) throw new Error("Loader was not initialized.");
-    return loader.loadDecisionTree("sampleDecisionTree");
+    return this.db.loadDecisionTree("sampleDecisionTree");
   }
 }
